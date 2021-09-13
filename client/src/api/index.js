f0876294a58772cec82fddcf885904291e8c445e
Prafull Sahu <psahu81011@gmail.com>
2021-09-13 17:01:13 +0530

    changing api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
-
-// const url = 'https://memories-project-prafull.herokuapp.com/posts';
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://memories-project-prafull.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    if (localStorage.getItem("profile")) {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
     }
 
     return req;
@@ -14,13 +13,12 @@ API.interceptors.request.use((req) => {
 
 export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 export const fetchPost = (id) => API.get(`/posts/${id}`);
-export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
-export const createPost = (newPost) => API.post('/posts', newPost);
+export const fetchPostsBySearch = (searchQuery) =>
+    API.get(`/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${searchQuery.tags}`);
+export const createPost = (newPost) => API.post("/posts", newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 
 export const signIn = (formData) => API.post(`/user/signin`, formData);
 export const signUp = (formData) => API.post(`/user/signup`, formData);
-
-
